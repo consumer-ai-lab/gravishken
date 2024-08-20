5560d60ce433be414a31819a9676aec15017c166
thrombe <yash.n.thombare620@gmail.com>
2024-08-21 04:20:52 +0530

    add ExeNotFound message varient

--- common/ts/types.ts
+++ common/ts/types.ts
@@ -2,22 +2,17 @@
 
 
 export enum Varient {
-    Var1 = 0,
-    Var2 = 1,
-    Err = 2,
-    Unknown = 3,
+    ExeNotFound = 0,
+    Err = 1,
+    Unknown = 2,
 }
 export interface Message {
     Type: Varient;
     Val: string;
 }
-export interface TVar1 {
-    Field1: number;
-    Field2: boolean;
-}
-export interface TVar2 {
-    Field1: boolean;
-    Field3: string;
+export interface TExeNotFound {
+    Name: string;
+    ErrMsg: string;
 }
 export interface TErr {
     Message: string;
