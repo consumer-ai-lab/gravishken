c18300b6a0f0007237814cf2a43b2df744f124e4
thrombe <yash.n.thombare620@gmail.com>
2024-08-22 02:16:55 +0530

    move -lpthread whack to new file (conditionally compiled)

--- application/src/main.go
+++ application/src/main.go
@@ -11,22 +11,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-/*
-// OOF: T_T T_T T_T this somehow fixes the compile issue on windows and i don't know why T_T T_T
-#cgo windows LDFLAGS: -static -lpthread
-
-#include <pthread.h>
-void* threadFunction(void* arg) {
-    return NULL;
-}
-void createThread(int* arg) {
-    pthread_t thread;
-    pthread_create(&thread, NULL, threadFunction, arg);
-    pthread_join(thread, NULL); // Wait for the thread to finish
-}
-*/
-import "C"
-
 var build_mode string
 var port string
 
