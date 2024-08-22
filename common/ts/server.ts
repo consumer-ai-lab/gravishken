b369581b118a6e2703e67eb2deca651b1e0bf887
thrombe <yash.n.thombare620@gmail.com>
2024-08-23 00:30:53 +0530

    impl common.server.Server.add_callback()

--- common/ts/server.ts
+++ common/ts/server.ts
@@ -1,4 +1,5 @@
 import * as types from "@common/types.ts"
+import { Mutex } from "./utils";
 
 export function exhausted(d: never) {
     console.log(d)
@@ -18,6 +19,16 @@ export type Message = {
     Typ: types.Varient.Unknown,
     Val: unknown,
 }
+
+// type ValType<T extends types.Varient> = Message extends { Typ: T; Val: infer V } ? V : never;
+type ValType<T extends types.Varient> = 
+    T extends types.Varient.ExeNotFound ? types.TExeNotFound :
+    T extends types.Varient.UserLogin ? types.TUserLogin :
+    T extends types.Varient.Err ? types.TErr :
+    unknown;
+
+type Callback<T extends types.Varient> = (res: ValType<T>) => PromiseLike<void>;
+type DisableCallback = () => Promise<void>;
 
 export class Server {
     ws: WebSocket;
@@ -49,8 +60,43 @@ export class Server {
         return self;
     }
 
+    // @ts-ignore
+    callbacks = new Map<Message["Typ"], [number, Callback<Message["Typ"]>][]>();
+    callback_mutex: Mutex = new Mutex();
+    callback_id = 1;
+    async add_callback<T extends Message["Typ"]>(type: T, cb: Callback<typeof type>): Promise<DisableCallback> {
+        let id = await this.callback_mutex.runExclusive(async () => {
+            let id = this.callback_id++;
+
+            let callbacks = this.callbacks.get(type) ?? null;
+            if (callbacks == null) {
+                this.callbacks.set(type, callbacks);
+            } else {
+                callbacks.push([id, cb]);
+            }
+
+            return id;
+        });
+
+        return async () => {
+            await this.callback_mutex.runExclusive(async () => {
+                let callbacks = this.callbacks.get(type) ?? [];
+                callbacks = callbacks.filter(e => e[0] != id);
+                this.callbacks.set(type, callbacks);
+            });
+        };
+    }
     async handle_message(msg: Message) {
         console.log(msg);
+
+        let callbacks = this.callbacks.get(msg.Typ) ?? [];
+        for (let callback of callbacks) {
+            await callback[1](msg.Val);
+        }
+        if (callbacks.length > 0) {
+            return;
+        }
+
         switch (msg.Typ) {
             case types.Varient.ExeNotFound:
             case types.Varient.Err:
