36b63f227619ad6adfdf88ccc467fb26d9392bb1
thrombe <yash.n.thombare620@gmail.com>
2024-09-03 03:40:13 +0530

    typing test submit moves to next test (temp)

--- frontend/src/components/typing-test.tsx
+++ frontend/src/components/typing-test.tsx
@@ -5,6 +5,8 @@ import { Textarea } from "@/components/ui/textarea";
 import { Progress } from "@/components/ui/progress";
 import { PlayCircle, StopCircle, Send } from 'lucide-react';
 import { match } from 'assert';
+import { server } from '@common/server.ts';
+import * as types from "@common/types.ts"
 
 const mockText = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
 
@@ -111,6 +113,11 @@ export default function TypingTest({
             rawWPM,
             accuracy: calculateAccuracy(inputText, mockText)
         });
+
+        // TODO: put this in a "Next section" button
+        server.send_message({Typ: types.Varient.LoadRoute, Val: {
+            Route: "/tests/2"
+        }});
     };
 
     const calculateAccuracy = (input:string, original:string) => {
